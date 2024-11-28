ab28ffe005497ab6a18fdf6c43f0d943eb0ec4d0
Lordfroze <yogaadiprtama22@gmail.com>
2024-11-28 20:05:50 +0700

    setup awal

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
-Route::get('/', [AdminController::class, 'index']);
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard/index');
 });
+
 
 
