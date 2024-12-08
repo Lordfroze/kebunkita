a34a4c09ee3532b6d6f1d7ecd9e18b63f59bf90e
froze <froze@debian>
2024-12-08 15:50:16 +0700

    pisah header footer

---
+++ resources/views/layouts/app/footer.blade.php
@@ -0,0 +1,26 @@
+<!-- Footer -->
+      <!-- Main Footer -->
+      <footer class="main-footer">
+        <!-- To the right -->
+        <div class="float-right d-none d-sm-inline">
+          solusi agribisnis milenial
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2024 <a href="#">Yogatama_KebunKita</a>.</strong> All rights reserved.
+      </footer>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('lte/dist/js/adminlte.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
+<!-- Footer End -->
