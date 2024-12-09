9493ac098fd8406a88ab9fe0de98a7991a7d1bb2
yoga <yogaadipratama22@gmail.com>
2024-12-09 13:47:34 +0700

    memisahkan setiap controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,38 +7,26 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     // Halaman home
-    function index(){
+    function index()
+    {
         return view('dashboard/index');
     }
 
-    // Halaman tabel ikan
-    function perikanan(){
-        return view('dashboard/perikanan/index');
-    }
-
-    // Halaman tabel perdagangan
-    function perdagangan(){
-        return view('dashboard/perdagangan/index');
-    }
-
-    // Halaman tabel perkebunan
-    function perkebunan(){
-        return view('dashboard/perkebunan/index');
-    }
-
-    // Halaman setting kolam
-    function settingkolam(){
+    // Halaman setting Perikanan
+    function settingkolam()
+    {
         return view('dashboard/perikanan/settingkolam');
     }
 
     //Halaman setting kebun
-    function settingkebun(){
+    function settingkebun()
+    {
         return view('dashboard/perkebunan/settingkebun');
     }
 
     //Halaman setting perdagangan
-    function settingbarang(){
+    function settingbarang()
+    {
         return view('dashboard/perdagangan/settingbarang');
     }
-
 }
