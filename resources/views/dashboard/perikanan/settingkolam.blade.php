d428cbacce16db66d75cc88eeb79bf2b62e63821
yoga <yogaadipratama22@gmail.com>
2024-12-09 07:31:06 +0700

    add active menu

--- resources/views/dashboard/perikanan/settingkolam.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('title')
-Setting Kolam
-@endsection
-
-@section('content')
-<button></button>
-@endsection
-
--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -1,4 +1,5 @@
 <!-- Header -->
+
 <body class="hold-transition sidebar-mini">
   <div class="wrapper">
 
@@ -143,7 +144,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="#" class="brand-link">
+      <a href="{{url('dashboard')}}" class="brand-link">
         <!-- <img src="{{ asset('lte/dist/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
         <span class="brand-text font-weight-light">Kebun Kita V.1</span>
       </a>
@@ -176,38 +177,43 @@
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-            <li class="nav-item menu-open">
-              <a href="#" class="nav-link active">
-                <i class="nav-icon fas fa-bars"></i>
-                <p>
-                  Home
-                  <i class="right fas fa-angle-left"></i>
-                </p>
+            <li class="nav-item {{ Request::is('dashboard*') ? 'menu-open' : '' }}"">
+              <a href=" #" class="nav-link {{ Request::is('dashboard*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-bars"></i>
+              <p>
+                Home
+                <i class="right fas fa-angle-left"></i>
+              </p>
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="#" class="nav-link active">
+                  <!-- <a href="#" class="nav-link active"> -->
+                  <a href="{{ url('dashboard/perikanan') }}" class="nav-link {{ Request::is('dashboard/perikanan*') ? 'active' : '' }}">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Perikanan</p>
                   </a>
                 </li>
+
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Perkebunan</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ url('dashboard/perdagangan') }}" class="nav-link {{ Request::is('dashboard/perdagangan*') ? 'active' : '' }}">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Perdagangan</p>
                   </a>
                 </li>
+
+                <li class="nav-item">
+                  <a href="{{ url('dashboard/perkebunan') }}" class="nav-link {{ Request::is('dashboard/perkebunan*') ? 'active' : '' }}">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Perkebunan</p>
+                  </a>
+                </li>
+
+
               </ul>
             </li>
 
-            <li class="nav-item menu-open">
-              <a href="#" class="nav-link active">
+            <li class="nav-item {{ Request::is('tambah-data*') ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ Request::is('tambah-data*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-bars"></i>
                 <p>
                   Tambah Data
@@ -216,19 +222,19 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="{{url('settingkolam')}}" class="nav-link">
+                  <a href="{{ url('tambah-data/perikanan') }}" class="nav-link {{ Request::is('tambah-data/perikanan*') ? 'active' : '' }}">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Perikanan</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ url('tambah-data/perikanan') }}" class="nav-link {{ Request::is('tambah-data/perikanan*') ? 'active' : '' }}">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Perkebunan</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ url('tambah-data/perikanan') }}" class="nav-link {{ Request::is('tambah-data/perikanan*') ? 'active' : '' }}">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Perdagangan</p>
                   </a>
@@ -237,7 +243,7 @@
             </li>
 
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="{{ url('unduh-data') }}" class="nav-link {{ Request::is('unduh-data*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-download"></i>
                 <p>
                   Unduh Data
@@ -259,7 +265,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0">Dashboard</h1>
+              <h1 class="m-0">@yield('title')</h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
