e2642d38dbad633585c37b756b8fb0b56d76ae90
Lordfroze <yogaadiprtama22@gmail.com>
2024-12-19 17:30:42 +0700

    add softdeletes

--- app/Models/Perikanan.php
+++ app/Models/Perikanan.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Perikanan extends Model
 {
     //
     Use HasFactory;
+    use SoftDeletes;
+    
     protected $table = 'perikanan';  // menggunakan tabel perikanan
     public function scopeActive($query)
     {
