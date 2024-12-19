d7bf0a2410d96c4a3594fd245adf3a84ea44389c
Lordfroze <yogaadiprtama22@gmail.com>
2024-12-20 06:43:04 +0700

    add auth

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,24 +10,40 @@ class DashboardController extends Controller
     // Halaman home
     function index()
     {
+        // otentikasi jika user belum login
+        if (!Auth::check()) {
+            return redirect('login');
+        }
         return view('dashboard/index');
     }
 
     // Halaman setting Perikanan
     function settingkolam()
     {
+        // otentikasi jika user belum login
+        if (!Auth::check()) {
+            return redirect('login');
+        }
         return view('dashboard/perikanan/settingkolam');
     }
 
     //Halaman setting kebun
     function settingkebun()
     {
+        // otentikasi jika user belum login
+        if (!Auth::check()) {
+            return redirect('login');
+        }
         return view('dashboard/perkebunan/settingkebun');
     }
 
     //Halaman setting perdagangan
     function settingbarang()
     {
+        // otentikasi jika user belum login
+        if (!Auth::check()) {
+            return redirect('login');
+        }
         return view('dashboard/perdagangan/settingbarang');
     }
 }
