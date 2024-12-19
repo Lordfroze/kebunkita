208047f99c59ca7e470571cd7b776e6722683c33
Lordfroze <yogaadiprtama22@gmail.com>
2024-12-19 19:24:49 +0700

    add komentar

--- app/Models/Perikanan.php
+++ app/Models/Perikanan.php
@@ -13,8 +13,20 @@ class Perikanan extends Model
     use SoftDeletes;
     
     protected $table = 'perikanan';  // menggunakan tabel perikanan
+    
     public function scopeActive($query)
     {
         return $query->where('active', 'true');
+    }
+
+    // menambah komentar
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // menambahkan total komentar
+    public function total_comments(){
+        return $this->comments()->count();
     }
 }
--- resources/views/dashboard/perikanan/show.blade.php
+++ resources/views/dashboard/perikanan/show.blade.php
@@ -11,6 +11,16 @@ Detail {{$task->kegiatan}}
         <p>Lokasi : {{$task->lokasi}}</p>
         <p>Biaya : {{$task->biaya}}</p>
         </article>
+
+        <small class="text-muted">{{$total_comments}} Komentar</small>        
+        <!-- Komentar -->
+         @foreach($comments as $comment)
+         <div class="card mb-3">
+            <div class="card-body">
+                <p style="font-size: 12pt">{{$comment->comment}}</p>
+            </div>
+         </div>
+         @endforeach
         <a href="{{ url('/dashboard/perikanan') }}">Kembali</a>
     </div>
 @endsection
