6e5b1606f7f4eb797da31f311ff0a2f3f1195ad2
Lordfroze <yogaadiprtama22@gmail.com>
2024-12-20 11:31:02 +0700

    add register form work

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+Use Illuminate\Support\Facades\Session;
+Use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-Use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
@@ -32,5 +34,28 @@ class AuthController extends Controller
         Session::flush();
         Auth::logout();
         return redirect('login');
+    }
+
+    public function register_form()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);    
+
+        // return redirect('login');
+        return redirect('login')->with('success_message', 'Register Success');
     }
 }
